a6a9763b588cb0253900afbd368149e37a57110b
GraeberFelix <felix.graeber@yahoo.de>
2017-11-10 17:29:22 +0100

    outsourced size vars to DataContainer; removed fleetsize calculation

--- src/logic/game.java
+++ src/logic/game.java
@@ -1,13 +1,9 @@
 public class game{
-	int x=0;
-	int y=0;
-	int fleetsize=0;
 	boolean is_online;
-	//konstruktor feldgröße, online/ singleplayer game
-	game(int x, int y,boolean o,){
-		this.x=x;
-		this.y=y;
+	//konstruktor feldgroesse, online/ singleplayer game
+	game(int x, int y, boolean o){
+		DataContainer.setSpielFeldBreite(x);
+		DataContainer.setSpielfeldHoehe(y);
 		is_online= o;
-        fleetsize= (int)(x*y*0.3);
 	} 
 }
