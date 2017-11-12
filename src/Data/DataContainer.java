535feabf578267e0841beefd47e64e9df73d4639
ChrisJK86 <33255748+ChrisJK86@users.noreply.github.com>
2017-11-12 14:20:04 +0100

    add some needed variables and getter + setter methods

--- src/Data/DataContainer.java
+++ src/Data/DataContainer.java
@@ -1,5 +1,9 @@
 package Data;
 
+import GUI.TableView;
+
+import javax.swing.*;
+
 /**
  * Diese Klasse beinhaltet sämtliche Werte, wie z.B. Spielfedgroesse. Ebenso enthaelt diese
  * Klasse Getter und Setter Methoden für saemtliche Werte.
@@ -10,35 +14,128 @@ package Data;
  */
 public class DataContainer {
 
-    private int spielFeldBreite = 10;
-    private int spielFeldHoehe = 10;
+    //Gametyp
+    private static String gameType = null;
+
+    //Spielfeld
+    private static int spielFeldBreite = 10;
+    private static int spielFeldHoehe = 10;
+
+    // Spieler Table
+    private static TableView table = null;
+
+    // ScrollPane
+    JScrollPane scrollPane = null;
+
+    // TextArea
+    JTextArea textArea = null;
+
+    // allowed variable dient zu prüfen ob man schießen darf oder nicht
+    private static boolean allowed;
+
 
 
     /*
     Gibt die Spielfeldbreite zurueck
      */
-    public int getSpielFeldBreite(){
+    public static int getSpielFeldBreite(){
         return  spielFeldBreite;
     }
 
     /*
     setzt die Spielfeldbreite
      */
-    public void setSpielFeldBreite(int n){
+    public static void setSpielFeldBreite(int n){
         spielFeldBreite = n;
     }
 
     /*
     gibt die Spielfeldhoehe zurueck
      */
-    public int getSpielFeldHoehe(){
+    public static int getSpielFeldHoehe(){
         return spielFeldHoehe;
     }
 
     /*
     setzt die Spielfeldhoehe
      */
-    public void setSpielFeldHoehe(int n){
+    public static void setSpielFeldHoehe(int n){
         spielFeldHoehe = n;
+    }
+
+
+    /*
+    @return Spielfeld Spieler
+     */
+    public static TableView getTable(){
+        return table;
+    }
+
+    public static void setTable(TableView tabelle){
+        table = tabelle;
+    }
+
+    /*
+    gibt den gameType zurück
+     */
+    public static String getGameType(){
+        return gameType;
+    }
+    /*
+    setzt den gameType
+     */
+    public static void setGameType(String typ){
+        gameType = typ;
+    }
+    /**
+     * Diese Methode setzt das uebergebene JScrollPane, welches unter den
+     * Spielfeldern angezeigt wird
+     *
+     * @param scrollPane
+     */
+    public void setScrollPane(JScrollPane scrollPane) {
+        this.scrollPane = scrollPane;
+    }
+
+    /**
+     * Diese Methode gibt das JScrollPane zurueck
+     *
+     * @return scrollPane
+     */
+    public JScrollPane getScrollPane() {
+        return scrollPane;
+    }
+    /**
+     * Diese Methode setzt die JTextArea, welche unter den Spielfeldern
+     * angezeigt wird
+     *
+     * @param textArea
+     */
+    public void setTextArea(JTextArea textArea) {
+        this.textArea = textArea;
+    }
+
+    /**
+     * Diese Methode gibt die JTextArea zurueck, welche unter den Spielfeldern
+     * angezeigt wird
+     *
+     * @return
+     */
+    public JTextArea getTextArea() {
+        return textArea;
+    }
+
+    /*
+    getter für die Variable allowed
+     */
+    public static boolean getAllowed(){
+        return allowed;
+    }
+
+    /*
+    setter für die Variable allowed
+     */
+    public static void setAllowed(boolean x){
+        allowed = x;
     }
 }
