f3b5226028acb915cc4de6ef2f682b0859feaa79
ChrisJK86 <33255748+ChrisJK86@users.noreply.github.com>
2017-11-26 16:29:00 +0100

    Menubar added

--- src/GUI/GameView.java
+++ src/GUI/GameView.java
@@ -5,6 +5,12 @@ import Data.DataContainer;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Diese Klasse bildet das eigentliche Spielfenster.
+ * Es enthält eine MenuBar um Spiele zu speichern/laden.
+ * Es enthält zwei Spielfelder, wobei das linke das des Spielers ist
+ * und das rechte ist das um Schüsse abzufeuern.
+ */
 public class GameView {
 
     private TableView tablePlayer;
@@ -17,6 +23,7 @@ public class GameView {
         playView.setSize((DataContainer.getSpielFeldBreite()*2 + 100), (DataContainer.getSpielFeldHoehe() + 100));
         playView.setBackground(Color.BLACK);
         playView.setForeground(Color.WHITE);
+        playView.setUndecorated(true);
         playView.setContentPane(Box.createVerticalBox());
 
         /**
@@ -27,12 +34,45 @@ public class GameView {
             tablePlayer = DataContainer.getTable();
 
         }else if(DataContainer.getGameType().equals("bdf")){ // bdf steht für Benutzerdefiniert
-            //only für Testzwecke die nächste zeile
-           // DataContainer.setTable(new TableView());
+
             tablePlayer = DataContainer.getTable();          // das Place ships window wird die Table anlegen
         }
         PlayerShootTable = new TableView();
 
+
+        /**
+         * JMenuBar mit den Unterpunkten zum Spiel speichern/laden und schließen.
+         */
+        JMenuBar bar = new JMenuBar();
+        {
+            JMenu menu = new JMenu("File");
+            {
+                JMenuItem item = new JMenuItem("Spiel laden");
+                item.addActionListener(
+                        (e) -> { //Todo Laden ausführen
+                             }
+                );
+                menu.add(item);
+            }
+            {
+                JMenuItem item = new JMenuItem("Spiel speichern");
+                item.addActionListener(
+                        (e) -> { //Todo speichern aufrufen
+                             }
+                );
+                menu.add(item);
+            }
+            {
+                JMenuItem item = new JMenuItem("Beenden");
+                item.addActionListener(
+                        (e) -> {
+                            playView.dispose();
+                        }
+                );
+                menu.add(item);
+            }
+            bar.add(menu);
+        }
         /**
          * ScrollPane und TextArea unterhalb des Spielfeldes für sämtliche Informationen
          */
@@ -53,10 +93,11 @@ public class GameView {
 
         Box hbox = Box.createHorizontalBox();
         hbox.add(tablePlayer);
-        hbox.add(Box.createHorizontalStrut(20));
+        hbox.add(Box.createHorizontalStrut(10));
         hbox.add(PlayerShootTable);
 
 
+        playView.setJMenuBar(bar);
         playView.add(hbox);
         playView.add(scrollPane);
         playView.pack();
