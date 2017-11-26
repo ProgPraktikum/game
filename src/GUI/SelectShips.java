b59f8f75b557202108b732026851f500c45004a8
ChrisJK86 <33255748+ChrisJK86@users.noreply.github.com>
2017-11-26 18:13:41 +0100

    -

--- src/GUI/SelectShips.java
+++ src/GUI/SelectShips.java
@@ -3,12 +3,18 @@ package GUI;
 import Data.DataContainer;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
 public class SelectShips {
+
+   private int belegung = 0;
+
 
     public SelectShips() {
 
@@ -20,11 +26,17 @@ public class SelectShips {
         selectships.setMinimumSize(new Dimension(400,400));
         selectships.setBackground(Color.BLACK);
 
+
+        /**
+        ArrayList nimmt die ganzen JSpinner auf welche für die Anzahl der Schiffe genutzt werden.
+         */
         List<JSpinner> spinners = new ArrayList<JSpinner>();
 
 
+        /**
+         *JLabel Waehle Anzahl Schiffe
+         */
 
-        // JLabel Waehle Anzahl Schiffe
         JLabel label1 = new JLabel("Waehle die Anzahl der Schiffe");
         label1.setForeground(Color.WHITE);
         label1.setBackground(Color.BLACK);
@@ -36,12 +48,16 @@ public class SelectShips {
 
         Box vbox_label = Box.createVerticalBox();
         Box vbox_spinner = Box.createVerticalBox();
+        Box horizont = Box.createHorizontalBox();
 
-			/*
+			/**
 			 * JLabel mit dem Text "Laenge ..." wird initialisiert
 			 * und zusätzlich jeweils ein spinner
 			 */
-        for (int i = DataContainer.getMaxShipLength(); i >=2; i--) {
+
+        for ( int i = DataContainer.getMaxShipLength(); i >=2; i--) {
+            int f = i;
+
             JLabel labelSize = new JLabel("Laenge " + i + " : ");
             labelSize.setForeground(Color.WHITE);
             labelSize.setBackground(Color.BLACK);
@@ -53,15 +69,25 @@ public class SelectShips {
             Size.setMinimumSize(new Dimension(50,25));
             Size.setMaximumSize(new Dimension(50,25));
             Size.setPreferredSize(new Dimension(50,25));
-
+            Size.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent e) {
+                    JSpinner s = (JSpinner) e.getSource();
+                    belegung = f* (int) s.getValue();
+                    System.out.println(belegung);
+                }
+            });
 
             vbox_spinner.add(Size);
+
 
 			/*
 			 * Der JSpinner wird zur Liste spinners hinzugefuegt
 			 */
             spinners.add(Size);
         }
+
+
 
 
         // Bestätigung Button
@@ -74,9 +100,9 @@ public class SelectShips {
         ok.addActionListener(
                 (e) -> {
                     DataContainer.setShipStack();
-                    if (DataContainer.setShipTypePush(spinners,
+                    if (DataContainer.setShipLengthPush(spinners,
                             DataContainer.getOccupancy())){
-
+                        int belegung = 0 ;
                         selectships.dispose();
                         new PlaceShips();
                     }else{
@@ -97,8 +123,19 @@ public class SelectShips {
         abbrechen.setFont(new Font("Tahoma", Font.PLAIN, 20));
         abbrechen.addActionListener(
                 (e) -> {
+                    belegung = 0;
                     selectships.dispose(); }
         );
+
+        /**
+         * Label für die anzeige der aktuellen Belegung
+         */
+
+            JLabel Beleg = new JLabel(belegung + "");
+            Beleg.setForeground(Color.WHITE);
+            Beleg.setBackground(Color.BLACK);
+            Beleg.setFont(new Font("Tahoma", Font.PLAIN, 30));
+
 
 
 
@@ -108,24 +145,28 @@ public class SelectShips {
 
 
         btn_box.add(ok);
-        btn_box.add(Box.createHorizontalStrut(2));
+       // btn_box.add(Box.createHorizontalStrut(2));
         btn_box.add(abbrechen);
 
         vbox.add(Box.createVerticalStrut(10));
         vbox.add(label1);
         vbox.add(Box.createVerticalStrut(20));
 
-        Box horizont = Box.createHorizontalBox();
+
+
         horizont.add(vbox_label);
         horizont.add(vbox_spinner);
+        horizont.add(Box.createHorizontalStrut(40));
+        horizont.add(Beleg);
         vbox.add(horizont);
 
         selectships.add(vbox);
-
+        selectships.add(Box.createVerticalStrut(20));
         selectships.add(btn_box);
         selectships.pack();
         selectships.setLocationRelativeTo(null);
         selectships.setVisible(true);
 
     }
+
 }
