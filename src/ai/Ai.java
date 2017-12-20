57d83cf0632674ef6bb9967cd5c81ce95abb7f53
Jan Riedel <jan.riedel@everbase.net>
2017-12-20 05:49:14 +0100

    Implement basic Ai firing evaluation

--- src/ai/Ai.java
+++ src/ai/Ai.java
@@ -1,18 +1,24 @@
 package ai;
 
+import java.util.ArrayList;
 import java.util.Random;
 import gameboard.Board;
 import data.Game;
+import util.Tuple;
 
 public class Ai {
     // MEMBER VARIABLES
+    private static Random randomGenerator = new Random();
+
+    private static Board aiBoard = new Board();
+    private static Board aiStrikes = new Board();
+
     private static int boardWidth;
     private static int boardHeight;
 
     private static boolean placed;
 
-    private static Board aiBoard = new Board();
-    private static Board aiStrikes = new Board();
+    private static Trace trace = new Trace();
 
     // CONSTRUCTOR
     public Ai() {
@@ -48,16 +54,130 @@ public class Ai {
     // PRIVATE METHODS
     private void eval() {
         /* evaluate next target, fire and handle results */
-        int x, y;
-        Trace trace = new Trace();
+        int x = 0, y = 0;
+
 
         if (trace.getSize() == 0) {
-            Random randomGenerator = new Random();
-            x = randomGenerator.nextInt(boardWidth);
-            y = randomGenerator.nextInt(boardHeight);
+            do {
+                x = randomGenerator.nextInt(boardWidth);
+                y = randomGenerator.nextInt(boardHeight);
+            } while (aiStrikes.getPlayershots(x, y) != -1);
+
         } else {
-            x = 0; // placeholder
-            y = 0; // placeholder
+            int traceLen = trace.getSize();
+            if (traceLen > 1) {
+                int x1 = trace.getTile(0)[0];
+                int x2 = trace.getTile(1)[0];
+                int y1 = trace.getTile(0)[1];
+                int y2 = trace.getTile(1)[1];
+
+                if (x1 == x2) {
+                    // orientation vertical
+                    int step = 0;
+                    int diff = x1 - x2;
+                    if (diff > 0) {
+                        // direction left
+                        if (diff == 1) {
+                            step = 2;
+                        }
+                        else if (diff >= 2) {
+                            step = 1;
+                        }
+
+                        while(aiStrikes.getPlayershots((x1 - step), y1) != -1 ) {
+                            step += 1;
+                        }
+                        x = x1 - step;
+                        y = y1;
+                    }
+                    else if (diff < 0) {
+                        // direction right
+                        if (diff == -1) {
+                            step = 2;
+                        }
+                        else if (diff <= 2) {
+                            step = 1;
+                        }
+
+                        while(aiStrikes.getPlayershots((x1 + step), y1) != -1 ) {
+                            step += 1;
+                        }
+                        x = x1 + step;
+                        y = y1;
+                    }
+                }
+                else if (y1 == y2) {
+                    //orientation horizontal
+                    int step = 0;
+                    int diff = y1 - y2;
+                    if (diff > 0) {
+                        // direction left
+                        if (diff == 1) {
+                            step = 2;
+                        }
+                        else if (diff >= 2) {
+                            step = 1;
+                        }
+
+                        while(aiStrikes.getPlayershots(x1, (y1 - step)) != -1 ) {
+                            step += 1;
+                        }
+                        x = x1;
+                        y = y1 - step;
+                    }
+                    else if (diff < 0) {
+                        // direction right
+                        if (diff == -1) {
+                            step = 2;
+                        }
+                        else if (diff <= 2) {
+                            step = 1;
+                        }
+
+                        while(aiStrikes.getPlayershots(x1, (y1 + step)) != -1 ) {
+                            step += 1;
+                        }
+                        x = x1;
+                        y = y1 + step;
+                    }
+                }
+            }
+            else if (traceLen == 1) {
+                // try any direction
+                int x1 = trace.getTile(0)[0];
+                int y1 = trace.getTile(0)[1];
+                int direction = randomGenerator.nextInt(3);
+                switch (direction) {
+                    case 0: // top
+                        if ((y1 - 1) >= 0 && aiStrikes.getPlayershots(x1, (y1 - 1)) == -1) {
+                            x = x1;
+                            y = y1 - 1;
+                            break;
+                        }
+                        // fall-through intended
+                    case 1: // right
+                        if ((x1 + 1) <= boardWidth && aiStrikes.getPlayershots((x1 + 1), y1) == -1) {
+                            x = x1 + 1;
+                            y = y1;
+                            break;
+                        }
+                        // fall-through intended
+                    case 2: // bottom
+                        if ((y1 + 1) <= boardHeight && aiStrikes.getPlayershots(x1, (y1 + 1)) == -1) {
+                            x = x1;
+                            y = y1 + 1;
+                            break;
+                        }
+                        // fall-through intended
+                    case 3: // left
+                        if ((x1 - 1) >= 0 && aiStrikes.getPlayershots((x1 - 1), y1) == -1) {
+                            x = x1 - 1;
+                            y = y1;
+                            break;
+                        }
+                }
+
+            }
         }
 
         /* Random randomGenerator = new Random();
@@ -65,24 +185,20 @@ public class Ai {
         y = randomGenerator.nextInt(boardHeight); */
 
         int ret = fire(x, y);    // 0: Wasser, 1: Treffer, 2: versenkt
-        System.out.println("before switch");
+        aiStrikes.setPlayershots(x, y, ret);
         switch (ret) {
             case 0:
                 System.out.println("Just water here :/");
                 break;
             case 1:
                 trace.addTile(x, y);
-                System.out.println("Hit ship!");
+                System.out.println("Hit ship! Size:" + trace.getSize());
                 break;
             case 2:
                 trace.clear();
                 System.out.println("Destroyed ship!");
                 break;
         }
-
-        /* util.Tuple[] traceTuple = trace.getTrace();
-        trace.getTrace()[0].get(0);
-        trace.getTrace()[0].get(1); */
     }
 
     private int fire(int x, int y) {
